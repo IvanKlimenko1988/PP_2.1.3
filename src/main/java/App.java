ce9ba6872de8e1f8088d5384ed0ddd246cb9f05f
dev.klimenko <i.klimenko.eng@gmail.com>
2023-03-26 15:52:28 +0400

    Task finished

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,19 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("hello_world");
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println("Ссылки been и bean1 ссылаются на один объект-бин? " + (bean == bean1));
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        cat1.setName("Мурка");
+        System.out.println(cat1.getName());
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        cat2.setName("Рыжик");
+        System.out.println(cat2.getName());
+        System.out.println("Ссылки cat1 и cat2 ссылаются на один объект-бин? " + (cat1 == cat2));
     }
 }
